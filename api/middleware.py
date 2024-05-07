25a68467b06a7077786a51bedbab97cad9155c7e
Arjun Manjunath <arjunmanjunath80@gmail.com>
2024-05-07 20:33:17 +0530

    Note: included middleware.py

---
+++ api/middleware.py
@@ -0,0 +1,25 @@
+from werkzeug.wrappers import Request, Response, ResponseStream
+
+
+class Middleware:
+    def __init__(self, app):
+        self.app = app
+        self.username = "rrtraders"
+        self.password = "pass123"
+        
+    def __call__(self, environ, start_response):
+        try: 
+            request = Request(environ)
+            username = request.authorization['username']
+            password = request.authorization['password']
+            if username == self.username and password == self.password:
+                environ['user'] = {
+                    'name': 'rrtraders'
+                }
+                return self.app(environ, start_response)
+            res = Response(u'Authorization failed', mimetype='text/plain', status=401)
+            return res(environ, start_response)
+        except Exception as e:
+            res = Response(u'Internal Error', mimetype='text/plain', status=500)
+            return res(environ, start_response)            
+        
